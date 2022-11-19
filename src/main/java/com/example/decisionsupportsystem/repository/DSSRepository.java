2331e3d31feefa4d7e6a25b0b1b95728229722af
TrungHieu-HCMUIT <19521515@gm.uit.edu.vn>
2022-11-19 14:32:28 +0700

    Init

---
+++ src/main/java/com/example/decisionsupportsystem/repository/DSSRepository.java
@@ -0,0 +1,7 @@
+package com.example.decisionsupportsystem.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DSSRepository {
+}
---
+++ src/main/java/com/example/decisionsupportsystem/service/DSSService.java
@@ -0,0 +1,8 @@
+package com.example.decisionsupportsystem.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DSSService {
+
+}
